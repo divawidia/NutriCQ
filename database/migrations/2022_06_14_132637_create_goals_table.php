276c7fb2de8a820c4f3cd90b8df08cba9be79697
DipaWidia <wiartha2001@gmail.com>
2022-06-14 21:28:46 +0800

    create and add goals table migration

---
+++ database/migrations/2022_06_14_132637_create_goals_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->double('air');
+            $table->double('energi');
+            $table->double('protein');
+            $table->double('lemak');
+            $table->double('karbohidrat');
+            $table->double('serat');
+            $table->double('abu');
+            $table->double('kalsium');
+            $table->double('fosfor');
+            $table->double('besi');
+            $table->double('natrium');
+            $table->double('kalium');
+            $table->double('tembaga');
+            $table->double('seng');
+            $table->double('retinol');
+            $table->double('b_karoten');
+            $table->double('karoten_total');
+            $table->double('thiamin');
+            $table->double('riboflamin');
+            $table->double('niasin');
+            $table->double('vitamin_c');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
