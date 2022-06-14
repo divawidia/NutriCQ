b07dd8fa158095e87b4bd8599d4b93244d8f6ca5
DipaWidia <wiartha2001@gmail.com>
2022-06-14 20:49:25 +0800

    add foods table

--- database/migrations/2022_06_14_123753_create_foods_table.php
+++ database/migrations/2022_06_14_123753_create_foods_table.php
@@ -15,7 +15,34 @@ class CreateFoodsTable extends Migration
     {
         Schema::create('foods', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('sumber');
+            $table->double('air');
+            $table->double('energi');
+            $table->double('protein');
+            $table->double('lemak');
+            $table->double('karbohidrat');
+            $table->double('serat');
+            $table->double('abu');
+            $table->double('kalsium');
+            $table->double('fosfor');
+            $table->double('besi');
+            $table->double('natrium');
+            $table->double('kalium');
+            $table->double('tembaga');
+            $table->double('seng');
+            $table->double('retinol');
+            $table->double('b_karoten');
+            $table->double('karoten_total');
+            $table->double('thiamin');
+            $table->double('riboflamin');
+            $table->double('niasin');
+            $table->double('vitamin_c');
+            $table->double('berat_per_takaran_saji');
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('food_categories')->onDelete('cascade');
         });
     }
 
