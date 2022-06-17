a882daf7fddef623132508b9b6ee40bf89b31d4a
agungleksono <agungleksono99@gmail.com>
2022-06-17 20:50:15 +0700

    create api routes for goal feature

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\GoalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('role', [Controller::class, 'index']);
+Route::get('/goal', [GoalController::class, 'index']);
+Route::get('/goal/{id}', [GoalController::class, 'show']);
+Route::post('/goal', [GoalController::class, 'store']);
+Route::put('/goal/{id}', [GoalController::class, 'update']);
+Route::delete('/goal/{id}', [GoalController::class, 'destroy']);
+
