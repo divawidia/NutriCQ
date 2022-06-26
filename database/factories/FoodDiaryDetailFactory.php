4c495f1611b0fe08eb7588241bfe0ae49e833e9a
DipaWidia <wiartha2001@gmail.com>
2022-06-27 00:23:12 +0800

    add food diary and food diary details factory

--- database/factories/FoodDiaryDetailFactory.php
+++ database/factories/FoodDiaryDetailFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Food;
+use App\Models\FoodDiary;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class FoodDiaryDetailFactory extends Factory
@@ -14,7 +16,34 @@ class FoodDiaryDetailFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'food_id' => function(){
+                return Food::factory()->create()->id;
+            },
+            'food_diary_id' => function(){
+                return FoodDiary::factory()->create()->id;
+            },
+            'air' => $this->faker->randomFloat(1),
+            'energi' => $this->faker->randomFloat(1),
+            'protein' => $this->faker->randomFloat(1),
+            'lemak' => $this->faker->randomFloat(1),
+            'karbohidrat' => $this->faker->randomFloat(1),
+            'serat' => $this->faker->randomFloat(1),
+            'abu' => $this->faker->randomFloat(1),
+            'kalsium' => $this->faker->randomFloat(1),
+            'fosfor' => $this->faker->randomFloat(1),
+            'besi' => $this->faker->randomFloat(1),
+            'natrium' => $this->faker->randomFloat(1),
+            'kalium' => $this->faker->randomFloat(1),
+            'tembaga' => $this->faker->randomFloat(1),
+            'seng' => $this->faker->randomFloat(1),
+            'retinol' => $this->faker->randomFloat(1),
+            'b_karoten' => $this->faker->randomFloat(1),
+            'karoten_total' => $this->faker->randomFloat(1),
+            'thiamin' => $this->faker->randomFloat(1),
+            'riboflamin' => $this->faker->randomFloat(1),
+            'niasin' => $this->faker->randomFloat(1),
+            'vitamin_c' => $this->faker->randomFloat(1),
+            'takaran_saji' => $this->faker->randomDigit()
         ];
     }
 }
