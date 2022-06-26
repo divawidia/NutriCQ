64f1d6eeb8e6c92d1a6d0a32f329030265398754
DipaWidia <wiartha2001@gmail.com>
2022-06-27 00:11:31 +0800

    define food diary relationship

--- app/Models/FoodDiary.php
+++ app/Models/FoodDiary.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FoodDiary extends Model
 {
     use HasFactory;
+    protected $table = 'food_diaries';
+
+    protected $guarded = [];
+
+    public $timestamps = false;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function foodDiaryDetails(): HasMany
+    {
+        return $this->hasMany(FoodDiaryDetail::class);
+    }
 }
--- app/Models/FoodDiaryDetail.php
+++ app/Models/FoodDiaryDetail.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FoodDiaryDetail extends Model
 {
     use HasFactory;
+
+    protected $table = 'food_diary_details';
+
+    protected $guarded = [];
+
+    public function foodDiary(): BelongsTo
+    {
+        return $this->belongsTo(FoodDiary::class);
+    }
 }
