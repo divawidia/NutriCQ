211a2e07248d6aa603939b82586614ac19609bbb
DipaWidia <wiartha2001@gmail.com>
2022-06-26 22:38:56 +0800

    add belongsTo relation in Food model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Food extends Model
 {
@@ -12,5 +14,10 @@ class Food extends Model
     protected $table = 'foods';
 
     protected $guarded = [];
+
+    public function foodCategory(): BelongsTo
+    {
+        return $this->belongsTo(FoodCategory::class, 'category_id', 'id');
+    }
 
 }
