73cf5011ed2b27dd421ea898cea2637f90ff9e74
andreanpradanaa <andreanpradana2001@gmail.com>
2022-06-29 05:48:47 +0700

    Add register controller

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register_user(Request $request)
+    {
+        $users = User::where('email', '=', $request->input('email'))->first();
+        if ($users != null) {
+            // User exist
+            return response([
+                'message' => "Email has been registered!"
+            ], 401);
+        }
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:3|confirmed',
+            // 'no_telp' => 'required|string',
+        ]);
+
+        // store data to database
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            // 'no_telp' => $request->no_telp,
+        ]);
+
+        // $user->attachRole($request->role_id);
+        $user->attachRole('user');
+
+        // result with token
+        $token = $user->createToken('user')->plainTextToken;
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'Successfull registgered',
+            'token' => $token,
+            'roles' => $user->roles->first()->name
+        ], 201);
+    }
+
+    public function register_doctor(Request $request)
+    {
+        $users = User::where('email', '=', $request->input('email'))->first();
+        if ($users != null) {
+            // User exist
+            return response([
+                'message' => "Email has been registered!"
+            ], 401);
+        }
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:3|confirmed',
+            // 'no_telp' => 'required|string',
+            // 'cv' => '',
+            // 'license' => '',
+        ]);
+
+        // store data to database
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            // 'no_telp' => $request->no_telp,
+            // 'cv' => $request->cv,
+            // 'license' => $request->license,
+        ]);
+
+        $user->attachRole('doctor');
+
+        // result with token
+        $token = $user->createToken('doctor')->plainTextToken;
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'Successfull registgered',
+            'token' => $token,
+            'roles' => $user->roles->first()->name
+        ], 201);
+    }
+}
