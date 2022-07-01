df5f9140ab2905c2dcfc7bb46354618c05e119b5
andreanpradanaa <andreanpradana2001@gmail.com>
2022-07-01 18:17:16 +0700

    Update RegisterController

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -12,6 +13,7 @@ class RegisterController extends Controller
     public function register_user(Request $request)
     {
         $users = User::where('email', '=', $request->input('email'))->first();
+        // Check if user exist or not
         if ($users != null) {
             // User exist
             return response([
@@ -23,6 +25,7 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:3|confirmed',
+            'no_telp' => 'required|string'
         ]);
 
         // store data to database
@@ -30,10 +33,9 @@ class RegisterController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => 'user'
+            'no_telp' => $request->no_telp
         ]);
 
-        // $user->attachRole($request->role_id);
         $user->attachRole('user');
 
         // result with token
@@ -60,8 +62,9 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:3|confirmed',
-            // 'cv' => 'required',
-            // 'license' => 'required',
+            'no_telp' => 'required|string',
+            'cv' => 'required|file',
+            'license' => 'required|file',
         ]);
 
         // store data to database
@@ -69,9 +72,9 @@ class RegisterController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => "doctor"
-            // 'cv' => $request->cv,
-            // 'license' => $request->license,
+            'no_telp' => $request->no_telp,
+            'cv' => $request->cv,
+            'license' => $request->license,
         ]);
 
         $user->attachRole('doctor');
