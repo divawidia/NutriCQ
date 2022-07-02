f8d10698846052b41dcc7955b22d70af92ea6ad4
DipaWidia <wiartha2001@gmail.com>
2022-07-02 22:32:21 +0800

    create FoodDiaryDetail belongs to relations

--- tests/Unit/FoodDiaryDetailTest.php
+++ tests/Unit/FoodDiaryDetailTest.php
@@ -2,7 +2,15 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Food;
+use App\Models\FoodCategory;
+use App\Models\FoodDiary;
+use App\Models\FoodDiaryDetail;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
 
 class FoodDiaryDetailTest extends TestCase
 {
@@ -11,8 +19,29 @@ class FoodDiaryDetailTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_food_diary_belongs_to_relations()
     {
-        $this->assertTrue(true);
+        $role = Role::factory()->create(['name' => 'user', 'display_name' => 'User', 'description' => 'User']);
+        $user = User::factory()->create();
+        $user->roles()->attach($role);
+
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+
+        $category = FoodCategory::factory()->create();
+        $food = Food::factory()->create(['category_id' => $category->id]);
+
+        $foodDiary = FoodDiary::factory()->create(['user_id' => $user->id]);
+        $foodDiaryDetail = FoodDiaryDetail::factory()->create([
+            'food_id' => $food->id,
+            'food_diary_id' => $foodDiary->id
+        ]);
+
+        $this->assertInstanceOf(Food::class, $foodDiaryDetail->foods);
+        $this->assertInstanceOf(FoodDiary::class, $foodDiaryDetail->foodDiary);
     }
 }
