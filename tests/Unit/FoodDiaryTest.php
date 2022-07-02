434b171b3832c3cab3b211cf698b74339ac73dba
DipaWidia <wiartha2001@gmail.com>
2022-07-02 22:22:09 +0800

    create unit test for FoodDiary model

--- tests/Unit/FoodDiaryTest.php
+++ tests/Unit/FoodDiaryTest.php
@@ -2,7 +2,16 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Food;
+use App\Models\FoodCategory;
+use App\Models\FoodDiary;
+use App\Models\FoodDiaryDetail;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
 
 class FoodDiaryTest extends TestCase
 {
@@ -11,8 +20,65 @@ class FoodDiaryTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_food_diary_has_many_food_diary_details()
     {
-        $this->assertTrue(true);
+        $category = FoodCategory::factory()->create();
+        $food = Food::factory()->create(['category_id' => $category->id]);
+        $foodDiary = FoodDiary::factory()->create();
+        $foodDiaryDetails = FoodDiaryDetail::factory()->create([
+            'food_diary_id' => $foodDiary->id,
+            'food_id' => $food->id
+        ]);
+
+        $this->assertInstanceOf(Collection::class, $foodDiary->foodDiaryDetails);
+        $this->assertInstanceOf(FoodDiaryDetail::class, $foodDiary->foodDiaryDetails->first());
+    }
+
+    public function test_food_diary_belongs_to_user()
+    {
+        $role = Role::factory()->create(['name' => 'user', 'display_name' => 'User', 'description' => 'User']);
+        $user = User::factory()->create();
+        $user->roles()->attach($role);
+
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+        $foodDiary = FoodDiary::factory()->create(['user_id' => $user->id]);
+
+        $this->assertInstanceOf(User::class, $foodDiary->user);
+    }
+
+    public function test_if_food_diary_deleted_then_all_food_diary_detail_will_be_deleted()
+    {
+        $role = Role::factory()->create(['name' => 'user', 'display_name' => 'User', 'description' => 'User']);
+        $user = User::factory()->create();
+        $user->roles()->attach($role);
+
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+        $category = FoodCategory::factory()->create();
+        $food = Food::factory()->create(['category_id' => $category->id]);
+
+        $foodDiary = FoodDiary::factory()->create(['user_id' => $user->id]);
+
+        $foodDiaryDetails1 = FoodDiaryDetail::factory()->create([
+            'food_id' => $food->id,
+            'food_diary_id' => $foodDiary->id
+        ]);
+        $foodDiaryDetails2 = FoodDiaryDetail::factory()->create([
+            'food_id' => $food->id,
+            'food_diary_id' => $foodDiary->id
+        ]);
+
+        $foodDiary->delete();
+
+        $this->assertDatabaseMissing('food_diaries', ['id' => $foodDiary->id]);
+        $this->assertDatabaseMissing('food_diary_details', ['id' => $foodDiaryDetails1->id]);
+        $this->assertDatabaseMissing('food_diary_details', ['id' => $foodDiaryDetails2->id]);
     }
 }
