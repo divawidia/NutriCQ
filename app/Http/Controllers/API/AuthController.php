05a0f4f971fb634b27f470130e30bae836ef1a9f
Wahyu <swahyu1412@gmail.com>
2022-07-03 19:43:57 +0700

    refactor login controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -11,13 +11,19 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //Login User
+    
+    public function index()
+    {
+        //This could be view file
+        return 'This is API for Login';
+    }
+
     public function indexUser()
     {
         return 'This is API page for Login User';
     }
 
-    public function loginUser(Request $request)
+    public function login(Request $request)
     {
         $fields = $request->validate([
             'email' => 'required|string',
@@ -42,46 +48,6 @@ class AuthController extends Controller
         ];
 
         return response($response, 201);
-    }
-
-    //Login ADMIN
-    public function indexAdmin()
-    {
-        return 'This is API page for Login Admin';
-    }
-
-    public function loginAdmin(Request $request)
-    {
-        $fields = $request->validate([
-            'email' => 'required|string',
-            'password' => 'required|string'
-        ]);
-
-
-        //Check email
-        $user = User::where('email', $fields['email'])->first();
-
-        //Check Password
-        if (!$user || !Hash::check($fields['password'], $user->password)) {
-            return response([
-                'message' => "Bad Crendential"
-            ], 401);
-        }
-
-        $token = $user->createToken('myapptoken')->plainTextToken;
-
-        $response = [
-            'user' => $user,
-            'token' => $token
-        ];
-
-        return response($response, 201);
-    }
-
-    public function index()
-    {
-        //This could be view file
-        return 'This is API for Login';
     }
 
     public function logout(Request $request)
