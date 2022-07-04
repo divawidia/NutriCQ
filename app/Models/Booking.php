10db08574aea4b43b3efdf02396bbc53f0becbb6
DipaWidia <wiartha2001@gmail.com>
2022-07-04 08:49:11 +0800

    refactor some factory models and feature test, create unit test for user and booking models

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
     use HasFactory;
+
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'nama_user',
+        'nama_dokter',
+        'tanggal',
+        'deskripsi',
+        'start_time',
+        'end_time',
+        'meeting_link',
+        'lama_meeting',
+        'file_resep',
+        'status',
+        'user_id',
+        'user_dokter_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- tests/Unit/BookingTest.php
+++ tests/Unit/BookingTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Booking;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class BookingTest extends TestCase
 {
@@ -11,8 +15,26 @@ class BookingTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_booking_belongs_to_user()
     {
-        $this->assertTrue(true);
+        $roleUser = Role::factory()->create(['name' => 'user', 'display_name' => 'User', 'description' => 'User']);
+        $roleDokter = Role::factory()->create(['name' => 'doctor', 'display_name' => 'Doctor', 'description' => 'Doctor']);
+
+        $userUser = User::factory()->create();
+        $userUser->roles()->attach($roleUser);
+
+        $userDokter = User::factory()->create();
+        $userDokter->roles()->attach($roleDokter);
+
+        $booking = Booking::factory()->create([
+            'nama_user' =>  $userUser->name,
+            'user_id' =>  $userUser->id,
+            'status' => 'Waiting',
+            'user_dokter_id' => $userDokter->id,
+            'nama_dokter' => $userDokter->name]);
+
+        $this->assertInstanceOf(User::class, $booking->user);
     }
 }
