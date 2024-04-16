5ea7eb6f4d899ac91bb21b044e5d43652b6d137c
divawidia <wiartha2001@gmail.com>
2024-04-16 21:40:52 +0800

    update food seeder

--- database/seeders/FoodSeeder.php
+++ database/seeders/FoodSeeder.php
@@ -16,15 +16,8 @@ class FoodSeeder extends Seeder
     {
         $sql = public_path('food_data.sql');
 
-        $db = [
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'host' => env('DB_HOST'),
-            'database' => env('DB_DATABASE')
-        ];
-
-        exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
-
-        \Log::info('SQL Import Done');
+        \DB::unprepared(
+            file_get_contents($sql)
+        );
     }
 }
