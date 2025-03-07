df38e7500903de4046e6789c51ce60e5df69e9a7
divawidia <wiartha2001@gmail.com>
2025-03-07 00:00:00 +0000

    refactor (db seeder): update user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(LaratrustSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(FoodCategoriesSeeder::class);
         $this->call(FoodSeeder::class);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'user']);
+        $role3 = Role::create(['name' => 'doctor']);
+
         $admin = User::create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
@@ -22,7 +27,25 @@ class UserSeeder extends Seeder
             'no_telp' => '08432432432',
             'status' => 'active'
         ]);
+        $admin->assignRole($role1);
 
-        $admin->attachRole('admin');
+        $admin = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user12345'),
+            'no_telp' => '08432432432',
+            'status' => 'active'
+        ]);
+        $admin->assignRole($role2);
+
+        $admin = User::create([
+            'name' => 'doctor',
+            'email' => 'doctor@gmail.com',
+            'password' => Hash::make('doctor12345'),
+            'no_telp' => '08432432432',
+            'status' => 'active'
+        ]);
+        $admin->assignRole($role3);
+
     }
 }
