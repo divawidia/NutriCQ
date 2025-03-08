aa1f614447faf10df72842ed0d34a40d949f8e24
divawidia <wiartha2001@gmail.com>
2025-03-08 00:00:00 +0000

    refactor (user controller): delete index method

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,17 +14,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        if(Auth::user()->hasRole('user')){
-            return 'This is protected dashboard for user only';
-        } elseif(Auth::user()->hasRole('doctor')) {
-            return 'This is protected dashboard for doctor only';
-        } elseif(Auth::user()->hasRole('admin')) {
-            return 'This is protected dashboard admin only';
-        }
-    }
-
     public function showUserProfile()
     {
         $user = auth()->user();
