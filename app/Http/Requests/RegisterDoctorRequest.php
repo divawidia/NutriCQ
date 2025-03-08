a2dd3f41ce22d9c5a3df4d243011539750046b31
divawidia <wiartha2001@gmail.com>
2025-03-08 00:00:00 +0000

    feat (register doctor request): add request validation

---
+++ app/Http/Requests/RegisterDoctorRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterDoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => ['required', 'string', Password::min(8)->letters()->mixedCase()->numbers(), 'confirmed'],
+            'tgl_lahir' => 'required|date|before:today',
+            'no_telp' => 'required|string|max:255',
+            'gender' => 'required|string|in:Laki-laki,Perempuan',
+            'cv' => 'required|file|mimes:pdf,doc,docx|max:2048',
+            'license' => 'required|file|mimes:pdf,doc,docx|max:2048',
+            'status' => 'required|string|in:Aktif,Tidak Aktif',
+        ];
+    }
+}
