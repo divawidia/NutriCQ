62a184413b84c277e81395a2b16efee010af6add
divawidia <wiartha2001@gmail.com>
2025-03-11 00:00:00 +0000

    feat (scramble): add authentication config

--- app/Http/Requests/UpdateUserProfileRequest.php
+++ app/Http/Requests/UpdateUserProfileRequest.php
@@ -3,11 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules\Password;
 
-class UpdateUserProfileRequest extends FormRequest
+    class UpdateUserProfileRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
