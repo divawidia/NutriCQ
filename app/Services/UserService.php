d2a345d4934fe74f18d3ad872d5c447a2e9f87e8
divawidia <wiartha2001@gmail.com>
2025-03-12 00:00:00 +0000

    feat (user service): update index method

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,18 +3,21 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Repositories\UserRepository;
 
 class UserService
 {
     protected GoalService $goalService;
-    public function __construct(GoalService $goalService)
+    protected UserRepository $userRepository;
+    public function __construct(GoalService $goalService, UserRepository $userRepository)
     {
         $this->goalService = $goalService;
+        $this->userRepository = $userRepository;
     }
 
-    public function index(string $search, string $gender, int $minHeight, int $maxHeight, int $minWeight, int $maxWeight, string $activityLevel, string $status)
+    public function index(string $search, string $gender, int $minHeight, int $maxHeight, int $minWeight, int $maxWeight, string $activityLevel, string $status, int $page, int $perPage)
     {
-
+        return $this->userRepository->index([], $search, $gender, $minHeight, $maxHeight, $minWeight, $maxWeight, $activityLevel, $status, $page, $perPage);
     }
 
     public function updateAuthUserProfile(array $data, User $user): User
