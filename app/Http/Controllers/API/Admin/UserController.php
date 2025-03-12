b41efdf5a2988974f4288f21548629b8b44c2180
divawidia <wiartha2001@gmail.com>
2025-03-12 00:00:00 +0000

    feat (admin user controller): add index methods

--- app/Http/Controllers/API/Admin/UserController.php
+++ app/Http/Controllers/API/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Admin;
 use App\Http\Controllers\Controller;
 use App\Services\UserService;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -13,13 +14,47 @@ class UserController extends Controller
     {
         $this->userService = $userService;
     }
-    
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        try {
+            $search = $request->get('search');
+            $gender = $request->get('gender');
+            $minHeight = $request->get('minHeight');
+            $maxHeight = $request->get('maxHeight');
+            $minWeight = $request->get('minWeight');
+            $maxWeight = $request->get('maxWeight');
+            $activityLevel = $request->get('activityLevel');
+            $status = $request->get('status');
+            $page = $request->get('page');
+            $perPage = $request->get('perPage');
+
+            $data = $this->userService->index($search, $gender, $minHeight, $maxHeight, $minWeight, $maxWeight, $activityLevel, $status, $page, $perPage);
+
+            return response()->json([
+                'success' => true,
+                'status' => Response::HTTP_OK,
+                'message' => 'Users data successfully retrieved',
+                'data' => $data->iteam(),
+                'pagination' => [
+                    'total' => $data->total(),
+                    'per_page' => $data->perPage(),
+                    'current_page' => $data->currentPage(),
+                    'last_page' => $data->lastPage(),
+                ]
+            ], Response::HTTP_OK);
+
+        }catch (\Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'status' => 500,
+                'message' => 'Something went wrong while retrieving user data',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
