e1bb485e798b1fccb99f31d44ad682d120245cbf
divawidia <wiartha2001@gmail.com>
2025-03-12 00:00:00 +0000

    feat (user repository): create index methods

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -13,6 +13,35 @@ class UserRepository
         $this->user = $user;
     }
 
+    public function index(
+        array $withRelations = [],
+        string $search = null,
+        string $gender = null,
+        int $minHeight = null,
+        int $maxHeight = null,
+        int $minWeight = null,
+        int $maxWeight = null,
+        string $activityLevel = null,
+        string $status = null,
+        int $page = 1,
+        int $perPage = 10
+    )
+    {
+        $skip = ($page - 1) * $perPage;
+        return $this->user->with($withRelations)
+            ->when($search, fn($query) => $query->search($search))
+            ->when($gender, fn($query) => $query->where('gender', $gender))
+            ->when($minHeight, fn($query) => $query->where('tinggi_badan', '>=',$minHeight))
+            ->when($maxHeight, fn($query) => $query->where('tinggi_badan', '<=', $maxHeight))
+            ->when($minWeight, fn($query) => $query->where('berat_badan', '>=',$minWeight))
+            ->when($maxWeight, fn($query) => $query->where('berat_badan', '<=', $maxWeight))
+            ->when($activityLevel, fn($query) => $query->where('tingkat_aktivitas', $activityLevel))
+            ->when($status, fn($query) => $query->where('status', $status))
+            ->when($page, fn($query) => $query->skip($skip))
+            ->when($status, fn($query) => $query->take($perPage))
+            ->get();
+    }
+
     /**
      * Create a new user with the provided data.
      *
